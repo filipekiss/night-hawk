83256b03ceca84bafdc27e979efed7e8e8732348
Filipe Kiss <hello@filipekiss.com.br>
2019-04-26 16:03:33 -0300

    Migrate Estilo generator to templates

--- src/scripts/generate-estilo-palette-from-json.ts
+++ src/scripts/generate-estilo-palette-from-json.ts
@@ -1,19 +1,10 @@
-import {save, fromRoot} from './utils/file';
+import {fromRoot} from './utils/file';
+import {getTemplates, saveOutputFile} from './utils/templates';
 
 import colors from '../colors.json';
 import semanticColors from '../semantic-colors.json';
 
 export default function exportEstiloPalette() {
-    const generationTime = new Date();
-    const colorsHeader = `# Night Hawk Palette
-# Ordered alphabetically
-# Auto-generated on ${generationTime
-        .toISOString()
-        .substring(0, 16)
-        .replace('T', ' ')
-        .replace(/\-/g, '/')}
-`;
-
     const printColorFormat = function(colorName, colorObject) {
         return `${colorName}: '${colorObject.hex}'`;
     };
@@ -24,11 +15,10 @@ export default function exportEstiloPalette() {
         }
     );
 
-    const filePath = fromRoot(`estilo/palettes/nighthawk.yml`);
-
-    const fileContents = `${colorsHeader}
-${colorStrings.join('\n')}
-`;
-
-    save(filePath, fileContents);
+    const templates = getTemplates(fromRoot('src/templates/estilo/palettes'));
+    saveOutputFile(
+        templates,
+        {colorsList: colorStrings.join('\n')},
+        {outDir: fromRoot('estilo/palettes')}
+    );
 }
